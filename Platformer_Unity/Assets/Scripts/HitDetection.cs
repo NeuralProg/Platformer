af5c039f658e45f61644a0ab0dfdd37f02e4f781
NeuralPrg <chabaneyann@gmail.com>
2023-02-22 12:46:22 +0100

    refresh trigger step1

--- Platformer_Unity/Assets/Scripts/HitDetection.cs
+++ Platformer_Unity/Assets/Scripts/HitDetection.cs
@@ -6,6 +6,43 @@ public class HitDetection : MonoBehaviour
 {
     public bool isPlayer;
 
+    private Vector2 colliderSize;
+    public bool canRefreshTrigger = true;
+
+    private void Start()
+    {
+        colliderSize = GetComponent<BoxCollider2D>().size;
+        InvokeRepeating("StartCoroutine(RefreshTrigger(Time.fixedDeltaTime))", 0f, Time.fixedDeltaTime * 2);
+    }
+
+    /*private void Update()
+    {
+        if (gameObject.name == "SlashDown")
+            print(gameObject.activeSelf);
+
+        if (gameObject.activeSelf)
+        {
+            if (canRefreshTrigger)
+            {
+                canRefreshTrigger = false;
+                StartCoroutine(RefreshTrigger(Time.fixedDeltaTime));
+            }
+        }
+        else
+        {
+            canRefreshTrigger = true;
+        }
+    }*/
+
+    private IEnumerator RefreshTrigger(float triggerRefreshRate)
+    {
+        GetComponent<BoxCollider2D>().size = new Vector2(0f, 0f);
+        yield return new WaitForSeconds(triggerRefreshRate);
+        GetComponent<BoxCollider2D>().size = colliderSize;
+        yield return new WaitForSeconds(triggerRefreshRate);
+    }
+
+
     private void OnTriggerEnter2D(Collider2D coll)
     {
         HitCharacter(coll);
@@ -38,10 +75,10 @@ public class HitDetection : MonoBehaviour
             }
             else
             {
-                if (collision.tag == "Knockback Block")
+                if (collision.tag == "Knockback Block" && gameObject.name == "SlashDown")
                 {
                     gameObject.transform.parent.gameObject.GetComponent<PlayerMovements>().ResetMechanics();
-                    gameObject.transform.parent.gameObject.GetComponent<PlayerMovements>().Knockback(gameObject.transform.parent.gameObject.transform.position - collision.transform.position, 0.2f);
+                    gameObject.transform.parent.gameObject.GetComponent<PlayerMovements>().Knockback(new Vector3(0f, Mathf.Abs(gameObject.transform.parent.gameObject.transform.position.y - collision.transform.position.y), 0f), 0.2f);
                 }
             }
         }
