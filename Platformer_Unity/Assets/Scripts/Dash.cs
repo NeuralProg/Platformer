ddecae8b8d40a6620ec20748c220e64266a80487
NeuralPrg <chabaneyann@gmail.com>
2023-02-26 15:38:53 +0100

    IA dash

--- Platformer_Unity/Assets/Scripts/Dash.cs
+++ Platformer_Unity/Assets/Scripts/Dash.cs
@@ -7,36 +7,39 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
-using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Dash : EnemyAction
 {
-    public float dashDuration = 0.4f;
-    public float dashForce = 10;
+    public float dashDuration;
+    public float dashForce;
+    public Transform checkEdge;
 
-    private bool isDashing = true;
-    private float dashVelocity;
+    private bool isDashing;
+
 
     public override void OnStart()
     {
+        isDashing = true;
         StartCoroutine(DashTimer());
+    }
+
+    public override void OnFixedUpdate()
+    {
+        rb.velocity = new Vector2(dashForce * (transform.localScale.x / 4) * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        var goingToGround = Physics2D.OverlapCircle(checkEdge.position, 0.04f, GetComponent<CyborgEnemy>().whatIsGround);
+
+        return !isDashing || !goingToGround ? TaskStatus.Success : TaskStatus.Running;
     }
 
     private IEnumerator DashTimer()
     {
         yield return new WaitForSeconds(dashDuration);
         isDashing = false;
-    }
-
-    public override void OnFixedUpdate()
-    {
-        rb.velocity = new Vector2((transform.localScale.x / 4) * dashVelocity, rb.velocity.y);
-    }
-
-    public override TaskStatus OnUpdate()
-    {
-        return isDashing ? TaskStatus.Running : TaskStatus.Success;
     }
 }
